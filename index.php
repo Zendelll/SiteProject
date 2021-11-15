a9023ac31386c2b6f9dc1a9b3197fc07e961b9b2
Zendell <zendelll@yandex.ru>
2021-11-16 01:43:11 +0300

    small changes

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Начальная страница</title>
+    <title>Добро пожаловать</title>
 </head>
 <body>
-
+    <h1>Начальная страница</h1>
 </body>
 </html>
